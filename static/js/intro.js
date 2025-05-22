717aec6a50eb5124e8ced9b557926f36b803e813
Justin Lai <laichimeng2001@gmail.com>
2025-05-22 09:21:38 +0800

    intra from center

--- static/js/intro.js
+++ static/js/intro.js
@@ -1,14 +1,43 @@
-
 // $('body').addClass("overflow-hidden");
-$('.intro-bar').each(function(i) {
-    setTimeout(() => {
-        $(this).addClass("!bg-lime-500")
-        setTimeout(() => {
-            $(this).css("opacity", "0");
-        }, i * 30);
-    }, i * 30);
-});
+$(document).ready(() => {
+    const len = $('.intro-bar').length;
 
-setTimeout(() => {
-    $('#intro').css("display", "none");
-}, $('.intro-bar').length * 90);
+    let pos = Math.floor(len / 2);
+    let neg = Math.floor(len / 2);
+    let curr = 0;
+    // inseatd .each, make it process from middle, to both end, like 9 bars will go from 5,6,4,7,3,8 ...
+    for (let i = 0; i < len; i++) {
+        setTimeout(() => {
+            if (i == 0) {
+                curr = neg;
+            }
+            else if (i % 2 == 0) {
+                pos += 1;
+                curr = pos;
+                console.log('pos', curr);
+            } else {
+                neg -= 1;
+                curr = neg;
+                console.log('neg', curr);
+            }
+
+            const bar = $('.intro-bar').eq(curr);
+            bar.addClass("!bg-lime-500");
+            setTimeout(() => {
+                bar.css("opacity", "0");
+            }, i * 20);
+        }, i * 20);
+    };
+    // $('.intro-bar').each(function(i) {
+    //     setTimeout(() => {
+    //         $(this).addClass("!bg-lime-500")
+    //         setTimeout(() => {
+    //             $(this).css("opacity", "0");
+    //         }, i * 30);
+    //     }, i * 30);
+    // });
+
+    setTimeout(() => {
+        $('#intro').css("display", "none");
+    }, $('.intro-bar').length * 90);
+});
